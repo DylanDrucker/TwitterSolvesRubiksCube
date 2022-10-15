d0d1296e8a094ba1505ee5b9e426cf253f5d55bd
Julia López Gómez <s2107370@ed.ac.uk>
2022-10-15 16:55:06 +0100

    Update solveCube.py

--- solveCube.py
+++ solveCube.py
@@ -1,60 +1,67 @@
 import rubikMoves
+import os
 
 def main():
     cube = rubikMoves.Rubik()
 
-    # Takes input from user
-    while(True):
-        move = input("What move do you want to take: ")
-        if move == "quit":
-            break
+    modified = os.path.getmtime("move.txt")
 
-        move = move.upper()
+    while True:
+        if modified != os.path.getmtime("move.txt"):
+            modified = os.path.getmtime("move.txt")
+            f = open("move.txt", "r")
+            move = f.read()
+            #move = input("What move do you want to take: ")
+            #if move == "quit":
+            #    break
 
-        if move == "U":
-            cube.u()
-        elif move == "D":
-            cube.d()
-        elif move == "R":
-            cube.r()
-        elif move == "L":
-            cube.l()
-        elif move == "F":
-            cube.f()
-        elif move == "B":
-            cube.b()
+            move = move.upper()
 
-        elif move == "U'":
-            cube.prime(cube.u)
-        elif move == "D'":
-            cube.prime(cube.d)
-        elif move == "R'":
-            cube.prime(cube.r)
-        elif move == "L'":
-            cube.prime(cube.l)
-        elif move == "F'":
-            cube.prime(cube.f)
-        elif move == "B'":
-            cube.prime(cube.b)
+            if move == "U":
+                cube.u()
+            elif move == "D":
+                cube.d()
+            elif move == "R":
+                cube.r()
+            elif move == "L":
+                cube.l()
+            elif move == "F":
+                cube.f()
+            elif move == "B":
+                cube.b()
 
-        elif move == "U2":
-            cube.twice(cube.u)
-        elif move == "D2":
-            cube.twice(cube.d)
-        elif move == "R2":
-            cube.twice(cube.r)
-        elif move == "L2":
-            cube.twice(cube.l)
-        elif move == "F2":
-            cube.twice(cube.f)
-        elif move == "B2":
-            cube.twice(cube.b)
+            elif move == "U'":
+                cube.prime(cube.u)
+            elif move == "D'":
+                cube.prime(cube.d)
+            elif move == "R'":
+                cube.prime(cube.r)
+            elif move == "L'":
+                cube.prime(cube.l)
+            elif move == "F'":
+                cube.prime(cube.f)
+            elif move == "B'":
+                cube.prime(cube.b)
+
+            elif move == "U2":
+                cube.twice(cube.u)
+            elif move == "D2":
+                cube.twice(cube.d)
+            elif move == "R2":
+                cube.twice(cube.r)
+            elif move == "L2":
+                cube.twice(cube.l)
+            elif move == "F2":
+                cube.twice(cube.f)
+            elif move == "B2":
+                cube.twice(cube.b)
+    
+
+            if move != "NO MOVE":
+                cube.fileOutput()
+                print(cube)
             
-        
-        
-        print(cube)
-        cube.fileOutput()
-            
+            print(move)
         
 
 
